fbc83da47bc25940558217ef0e72619e8d482967
Markus knauer <knauer.2009@hotmail.com>
2022-10-19 21:27:37 -0300

    Criando a classe Posicao(19/10/2022)

---
+++ xadrez-console/xadrez-console/anotações/Xadrez - Criando a primeira classe do projeto.cs
@@ -0,0 +1,21 @@
+/*
+// CRIANDO A PRIMEIRA CLASSE DO PROJETO:
+
+	-	Padrão camadas
+	-	Criamos a classe Posicao
+	-	Técnicas de orientação a objetos aplicadas:
+		*	Encapsulamento
+				{get; set;}
+		
+		*	Construtores  
+				public Posicao(int linha, int coluna){ }
+		
+		*	Palavra this(autorreferencia)
+				this.linha = linha;
+				this.coluna = coluna;
+	
+		*	Sobreposição(ToString)
+				override
+	-	Commit: "Criando a classe Posicao"
+
+ */
