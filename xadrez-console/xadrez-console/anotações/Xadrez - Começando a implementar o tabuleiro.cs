fa5ba296ea316cfcc3902b67d36d38ddcfddc0a5
Markus knauer <knauer.2009@hotmail.com>
2022-10-19 21:44:49 -0300

    Começando a implementar o tabuleiro

---
+++ xadrez-console/xadrez-console/anotações/Xadrez - Começando a implementar o tabuleiro.cs
@@ -0,0 +1,13 @@
+﻿/*
+    // COMEÇANDO A IMPLEMENTAR O TABULEIRO
+
+    -   Técnicas de orientação a objetos aplicadas:
+        *   Tipos enumerados(Cor.cs)
+        *   Associações
+        *   Encapsulamento(Novo: protected)
+    
+    -   Estruturas de dados aplicadas:
+        *   Matriz;
+    
+    -   Commit: "Começando a implementar o tabuleiro"
+ */
