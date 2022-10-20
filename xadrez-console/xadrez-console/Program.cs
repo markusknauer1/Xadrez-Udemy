83217d5c6c331cd5b9545a5fe41f0f038afba36e
Markus knauer <knauer.2009@hotmail.com>
2022-10-19 21:11:07 -0300

    Att

---
+++ xadrez-console/xadrez-console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+            Console.WriteLine();
+        }
+    }
+}
