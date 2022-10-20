202095c2cfbb4e159159f5e24dff74d126f179bc
Markus knauer <knauer.2009@hotmail.com>
2022-10-20 15:42:23 -0300

    CRIANDO A CLASSE POSICAOXADREZ

---
+++ xadrez-console/xadrez-console/anotações/Xadrez - Criando a classe PosicaoXadrez.cs
@@ -0,0 +1,13 @@
+﻿
+/*
+    //  CRIANDO A CLASSE POSICAOXADREZ
+
+    -   Criamos a classe PosicaoXadrez;
+    -   Incluimos um método que a converte para Posicao;
+    -   Técnicas de Orientação a objetos aplicadas:
+        *   Encapsulamento
+        *   Construtores
+        *   palavra this(autorrefencia)
+        *   Sobreposição(ToString)
+    -   Commit:"Criando a classe PosicaoXadrez;
+ */
