8a0edb68ae9562aa19b15db7292f5fa3a6c5fb25
Markus knauer <knauer.2009@hotmail.com>
2022-10-20 15:08:24 -0300

    Tratando erros com exceção personalizada

--- xadrez-console/xadrez-console/Program.cs
+++ xadrez-console/xadrez-console/Program.cs
@@ -8,29 +8,39 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
-            // cor Preta
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Cavalo(tab, Cor.Preta), new Posicao(0, 1));
-            tab.colocarPeca(new Bispo(tab, Cor.Preta), new Posicao(0, 2));
-            tab.colocarPeca(new Rainha(tab, Cor.Preta), new Posicao(0, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 4));
-            tab.colocarPeca(new Bispo(tab, Cor.Preta), new Posicao(0, 5));
-            tab.colocarPeca(new Cavalo(tab, Cor.Preta), new Posicao(0, 6));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,7));
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                // cor Preta
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Cavalo(tab, Cor.Preta), new Posicao(0, 1));
+                tab.colocarPeca(new Bispo(tab, Cor.Preta), new Posicao(0, 2));
+                tab.colocarPeca(new Rainha(tab, Cor.Preta), new Posicao(0, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 4));
+                tab.colocarPeca(new Bispo(tab, Cor.Preta), new Posicao(0, 5));
+                tab.colocarPeca(new Cavalo(tab, Cor.Preta), new Posicao(0, 6));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 7));
 
-            // cor Branca
-            tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 0));
-            tab.colocarPeca(new Cavalo(tab, Cor.Branca), new Posicao(7, 1));
-            tab.colocarPeca(new Bispo(tab, Cor.Branca), new Posicao(7, 2));
-            tab.colocarPeca(new Rainha(tab, Cor.Branca), new Posicao(7, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(7, 4));
-            tab.colocarPeca(new Bispo(tab, Cor.Branca), new Posicao(7, 5));
-            tab.colocarPeca(new Cavalo(tab, Cor.Branca), new Posicao(7, 6));
-            tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 7));
+                // cor Branca
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 0));
+                tab.colocarPeca(new Cavalo(tab, Cor.Branca), new Posicao(7, 1));
+                tab.colocarPeca(new Bispo(tab, Cor.Branca), new Posicao(7, 2));
+                tab.colocarPeca(new Rainha(tab, Cor.Branca), new Posicao(7, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(7, 4));
+                tab.colocarPeca(new Bispo(tab, Cor.Branca), new Posicao(7, 5));
+                tab.colocarPeca(new Cavalo(tab, Cor.Branca), new Posicao(7, 6));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 7));
 
 
-            Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
 
 
             Console.WriteLine();
---
+++ xadrez-console/xadrez-console/anotações/Xadrez - Criando uma exceção personalizada de tabuleiro.cs
@@ -0,0 +1,11 @@
+﻿
+/*
+    //  CRIANDO UMA EXCEÇÃO PERSONALIZADA DE TABULEIRO
+    //  TESTANDO A EXCEÇÃO PERSONALIZADA DE TABULEIRO
+
+    -   Técnicas de orientação a objetos aplicadas:
+        *   Sobrecarga(obter uma peça por Posicao
+        *   Exceções
+    
+    -   Commit: "Tratando erros com exceção personalizada"
+ */
