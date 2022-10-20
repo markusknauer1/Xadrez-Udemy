6f6f92df769a2ebd4ca7d27476b15b58c698bc61
Markus knauer <knauer.2009@hotmail.com>
2022-10-20 14:43:57 -0300

    Começando a colocar as peças no tabuleiro

---
+++ xadrez-console/xadrez-console/anotações/Xadrez - Começando a colocar peças no tabuleiro.cs
@@ -0,0 +1,13 @@
+﻿
+/*
+    // COMEÇANDO A COLOCAR PEÇAS NO TABULEIRO
+
+    -   Técnicas de orientação a objetos aplicadas:
+        *   herança(Rei é uma Peca(subClasse), Torre é uma Peca(subClasse);
+        *   associações(a Peca tem um Tabuleiro, o Tabuleiro tem varias Pecas;
+        *   construtor
+        *   :base
+    
+    -   Commit: "Começando a colocar as peças no tabuleiro"
+        
+ */
