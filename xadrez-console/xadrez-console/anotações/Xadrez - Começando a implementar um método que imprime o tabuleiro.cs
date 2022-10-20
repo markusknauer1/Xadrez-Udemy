a12686b3d296b2a3c914861edf0e5eb43d090aa9
Markus knauer <knauer.2009@hotmail.com>
2022-10-20 12:33:15 -0300

    começando a implementar um metodo que imprime o tabuleiro

---
+++ xadrez-console/xadrez-console/anotações/Xadrez - Começando a implementar um método que imprime o tabuleiro.cs
@@ -0,0 +1,15 @@
+﻿
+/*
+    // COMEÇANDO A IMPLEMENTAR UM MÉTODO QUE IMPRIME O TABULEIRO NA TELA ( TELA.CS )
+
+
+    -   Tecnicas de orientação a objetos aplicadas:
+        *   elementos estáticos
+        *   encapsulamento(método de acesso a um elemento da matriz)
+    
+    -   Commit:"COMEÇANDO A IMPLEMENTAR UM METODO QUE IMPRIME O TABULEIRO"
+
+
+    
+ 
+ */
