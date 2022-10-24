240191522db66bf9b12c79b157f3c4f17cde0e99
Markus knauer <knauer.2009@hotmail.com>
2022-10-24 17:09:45 -0300

    Implementando as restrições de movimento

---
+++ xadrez-console/xadrez-console/anotações/Xadrez - passagem de turno e.cs
@@ -0,0 +1,43 @@
+﻿/*
+    // passando turno
+        
+    Começamos o método realizarJogada
+    
+    Aplicação informando o turno e jogador atual
+
+    Técnicas de orientação a objetos aplicadas:
+        Encapsulamento(mudamos as restrições de aceso de alguns atributos - turno e jogadorAtual)
+
+
+
+    //  Validando a posição de origem
+
+    método ExisteMovimentosPossiveis(peca)
+    método validarPosicaoDeOrigem(PartidaDeXadrez)
+    fizemos o tratamento de excessões no programa principal
+
+    Técnicas de orientação a objetos aplicadas:
+        Exceções
+
+    Estruturas de dados aplicados:
+        Matriz
+
+
+    // Implementando as restrições de movimento
+    
+        Validando a posição de destino
+
+        Método podeMoverPara(peca)
+        
+        Método validarPosicaoDeDestino(PartidaDeXadrez)
+    
+        fizemos o tratamento de exceções no programa principal
+    
+        
+        Técnicas de orientação a objetos aplicadas;
+            Exceções
+        estruturas de dados aplicadas:
+            Matriz
+
+        Commit:"Implementando as restrições de movimento"
+ */
--- xadrez-console/xadrez-console/xadrez/Peao.cs
+++ xadrez-console/xadrez-console/xadrez/Peao.cs
@@ -21,6 +21,45 @@ namespace xadrez_console
             bool[,] mat = new bool[tab.linhas, tab.colunas];
             Posicao pos = new Posicao(0, 0);
 
+            // acima
+            pos.definirValores(posicao.linha - 2, posicao.coluna);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                pos.linha = pos.linha + 1;
+            }
+            //ne
+            pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            //se
+            pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            // abaixo
+            pos.definirValores(posicao.linha + 2, posicao.coluna);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                
+                pos.linha = pos.linha - 1;
+            }
+            //so
+            pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            //no
+            pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
             return mat;
         }
     }
