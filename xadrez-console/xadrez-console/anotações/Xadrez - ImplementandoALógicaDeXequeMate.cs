20a57dfa07e12e3f398295aa73076571dc545f7e
Markus knauer <knauer.2009@hotmail.com>
2022-10-26 14:40:30 -0300

    Implementando a lógica de xequemate

---
+++ xadrez-console/xadrez-console/anotações/Xadrez - ImplementandoALógicaDeXequeMate.cs
@@ -0,0 +1,19 @@
+﻿/*
+    //  Implementando a lógica de xequemate
+
+            Método testeXequeMate(PartidaDeXadrez)
+
+            Estruturas de dados aplicadas:
+                -   matrizes
+
+            Commit:(Ainda não)
+
+
+    //  Implementando a lógica de xequemate
+
+            Adaptações na aplicação para mostrar o xequemate
+
+            Correção no método testeXequeMate
+
+        commit;"Implementando a lógica de xequemate"
+ */
--- xadrez-console/xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -73,8 +73,18 @@ namespace xadrez
                 xeque = false;
             }
 
-            Peca p = tab.peca(destino);
-            mudaJogador();
+            //se o adversario estiver em xeque:
+            if (testeXqueMate(adversaria(jogadorAtual)))
+            {
+                terminada = true;
+            }
+            // se não estiver(passa o turno):
+            else
+            {
+                turno++;
+                mudaJogador();
+            }
+
            
         }
 
@@ -183,6 +193,37 @@ namespace xadrez
             return false;
         }
 
+        public bool testeXqueMate(Cor cor)
+        {
+            if (!estaEmXeque(cor))
+            {
+                return false;
+            }
+            foreach(Peca x in pecasEmJogo(cor))
+            {
+                bool[,] mat = x.movimentosPossiveis();
+                for(int i= 0; i < tab.linhas; i++)
+                {
+                    for (int j = 0; j < tab.colunas; j++)
+                    {
+                        if (mat[i, j])
+                        {
+                            Posicao origem = x.posicao;
+                            Posicao destino = new Posicao(i, j);
+                            Peca pecaCapturada = executaMovimento(origem, destino);
+                            bool testeXeque = estaEmXeque(cor);
+                            desfazMovimento(origem, destino, pecaCapturada);
+                            if (!testeXeque)
+                            {
+                                return false;
+                            }
+                        }
+
+                    }
+                }
+            }
+            return true;
+        }
         public void colocarNovaPeca(char coluna, int linha, Peca peca)
         {
             tab.colocarPeca(peca, new PosicaoXadrez(coluna, linha).toPosicao());
@@ -193,19 +234,12 @@ namespace xadrez
         {
 
             colocarNovaPeca('c', 1, new Torre(tab, Cor.Branca));
-            colocarNovaPeca('c', 2, new Torre(tab, Cor.Branca));
-            colocarNovaPeca('d', 2, new Torre(tab, Cor.Branca));
-            colocarNovaPeca('e', 2, new Torre(tab, Cor.Branca));
-            colocarNovaPeca('e', 1, new Torre(tab, Cor.Branca));
             colocarNovaPeca('d', 1, new Rei(tab, Cor.Branca, this));
+            colocarNovaPeca('h', 7, new Torre(tab, Cor.Branca));
 
 
-            colocarNovaPeca('c', 8, new Torre(tab, Cor.Preta));
-            colocarNovaPeca('c', 7, new Torre(tab, Cor.Preta));
-            colocarNovaPeca('d', 7, new Torre(tab, Cor.Preta));
-            colocarNovaPeca('e', 7, new Torre(tab, Cor.Preta));
-            colocarNovaPeca('e', 8, new Torre(tab, Cor.Preta));
-            colocarNovaPeca('d', 8, new Rei(tab, Cor.Preta, this));
+            colocarNovaPeca('a', 8, new Rei(tab, Cor.Preta, this));
+            colocarNovaPeca('b', 8, new Torre(tab, Cor.Preta));
 
             /*
              colocarNovaPeca('a', 1, new Torre(tab, Cor.Branca));
